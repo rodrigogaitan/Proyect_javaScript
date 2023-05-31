47aafaa9c13afa63088003b27edb952e7c3de162
rodrigogaitan <rodrigogaitan28@gmail.com>
2023-05-31 19:32:56 -0300

    Ejercicio 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+const nombre = 'Rodrigo'
+const edad = 22
+const desarrollador = false
+const nac = new Date('november 26 2000')
+const libro = {
+    titulo : 'Sexta Via',
+    autor : 'Patricio Sturlese',
+    fecha : 2010,
+    url : 'https//www.google.com'
+}
