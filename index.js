8a567c2e432c302a2145997cb970f2bd123dc3c6
rodrigogaitan <rodrigogaitan28@gmail.com>
2023-06-06 19:30:47 -0300

    Ejercicio 3

--- index.js
+++ index.js
@@ -1,10 +1,76 @@
-const nombre = 'Rodrigo'
-const edad = 22
-const desarrollador = false
-const nac = new Date('november 26 2000')
-const libro = {
-    titulo : 'Sexta Via',
-    autor : 'Patricio Sturlese',
-    fecha : 2010,
-    url : 'https//www.google.com'
-}
+// const nombre = 'Rodrigo'
+// const edad = 22
+// const desarrollador = false
+// const nac = new Date('november 26 2000')
+// const libro = {
+//     titulo : 'Sexta Via',
+//     autor : 'Patricio Sturlese',
+//     fecha : 2010,
+//     url : 'https//www.google.com'
+// }
+
+// const disponible = 100
+// const extraer = 70
+
+// if (extraer < disponible) {
+//     console.log('Puedes extraer');
+// } else {
+//     console.log('No puedes extraer');
+// }
+
+// for (let i = 1; i < 10; i++){
+    //     console.log(i);
+    // }
+    
+    // const developer = {
+    //     nombre: 'Rodrigo',
+    //     edad: 22,
+    //     mail: 'rodrigo@gmail.com'
+    // }
+    
+    // for (const propiedades in developer) {
+    //     console.log(developer[propiedades]);
+    // }
+
+// const numero = 3
+
+// if (numero === 3) {
+//     console.log('Advinaste');
+// } else if (numero === 2) {
+//     console.log('Casi');
+// } else if ( numero === 1) {
+//     console.log('Casi casi');
+// }else {
+//     console.log('Esta entre el 1 y el 3');
+// }
+// let i = 0
+// let max = 8
+
+// while (i < max) {
+//     console.log(i);
+//     i++;
+// }
+
+//let factorial = 1
+// for(let i = 10; i > 0; i-- ) {
+//     factorial *= i
+// }
+
+// console.log(factorial);
+
+// let factoria = 1
+// let num = 10
+// while (num > 1) {
+//     factoria *= num
+//     num--
+// }
+// console.log(factoria)
+
+// let factori = 1
+// let nums = 10
+// while (true) {
+//     factori *= nums
+//     nums--
+//     if (nums === 1) break
+// }
+// console.log(factori) 
